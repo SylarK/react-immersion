d35d61b07689405685a0db9311dd6f26137083a0
SylarK <syst.lucasamado@gmail.com>
2020-07-28 17:04:09 -0300

    Alteração no tamanho de algumas imagens. Alteração no footer. Alteração no arquivo JSON

--- amadoflix/src/App.js
+++ amadoflix/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"This comprehensive introduction to React was created by Bob Ziroll (@bobziroll). Bob is the Director of Education at V School, an award-winning school of technology education."}
       />
 
       <Carousel
--- amadoflix/src/components/Footer/index.js
+++ amadoflix/src/components/Footer/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { FooterBase } from './styles';
 
+const divLogo = {
+
+  width:'160px',
+  height: 'auto'
+
+}
+
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://www.linkedin.com/in/lucas-amado-335056167/">
+        <img style={divLogo} src="https://fontmeme.com/permalink/200728/aef60db7d917265569842919ce44dd6a.png" alt="Logo Amado" />
       </a>
       <p>
         Orgulhosamente criado durante a
